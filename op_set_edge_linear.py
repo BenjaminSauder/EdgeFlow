8f1be15e4f95a006a5651905b071edd6f8f5e9f6
Benjamin Sauder <13512160+BenjaminSauder@users.noreply.github.com>
2023-04-13 22:16:50 +0200

    * make operators repeatable

--- op_set_edge_linear.py
+++ op_set_edge_linear.py
@@ -13,33 +13,33 @@ class SetEdgeLinearOP(bpy.types.Operator, op_set_edge_flow.SetEdgeLoopBase):
     bl_description = "makes edge loops linear"
 
     space_evenly : BoolProperty(name="Space evenly", default=False)
-    distance : FloatProperty(name="Distance", default=1.0, min=0)
+    # distance : FloatProperty(name="Distance", default=1.0, min=0)
 
     def draw(self, context):
         layout = self.layout
         column = layout.column()
-        if self.do_straighten:
-            column.prop(self, "distance")
-        else:
-            column.prop(self, "space_evenly")
+        # if self.do_straighten:
+        #     column.prop(self, "distance")
+        # else:
+        column.prop(self, "space_evenly")
 
     def invoke(self, context, event):
         #print("--------------------")
         super(SetEdgeLinearOP, self).invoke(context)
 
-        self.do_straighten = self.can_straighten()
-        if self.do_straighten:
-            distance = 0
-            edge_count = 0
-            for obj in self.objects:
-                for edgeloop in self.edgeloops[obj]:
-                    distance += edgeloop.get_average_distance()
+        # self.do_straighten = self.can_straighten()
+        # if self.do_straighten:
+        #     distance = 0
+        #     edge_count = 0
+        #     for obj in self.objects:
+        #         for edgeloop in self.edgeloops[obj]:
+        #             distance += edgeloop.get_average_distance()
 
-                edge_count += len(self.edgeloops[obj])
+        #         edge_count += len(self.edgeloops[obj])
 
-            distance /= edge_count
+        #     distance /= edge_count
 
-            self.distance = distance * 0.35
+        #     self.distance = distance * 0.35
 
         return self.execute(context)
 
@@ -54,8 +54,12 @@ class SetEdgeLinearOP(bpy.types.Operator, op_set_edge_flow.SetEdgeLoopBase):
     def execute(self, context):
         #print("execute")
         
-        if not hasattr(self, "objects") or not self.objects:
+        # if not hasattr(self, "objects") or not self.objects:
+            # return self.invoke(context, None)
+        if not self.is_invoked:        
             return self.invoke(context, None)
+
+        bpy.ops.object.mode_set(mode='OBJECT')
 
         bpy.ops.object.mode_set(mode='OBJECT')
 
@@ -63,12 +67,13 @@ class SetEdgeLinearOP(bpy.types.Operator, op_set_edge_flow.SetEdgeLoopBase):
 
         for obj in self.objects:
             for edgeloop in self.edgeloops[obj]:
-                if self.do_straighten:
-                    edgeloop.straighten(self.distance)
-                else:
-                    edgeloop.set_linear(self.space_evenly)
+                # if self.do_straighten:
+                #     edgeloop.straighten(self.distance)
+                # else:
+                edgeloop.set_linear(self.space_evenly)
 
             self.bm[obj].to_mesh(obj.data)
 
         bpy.ops.object.mode_set(mode='EDIT')
+        self.is_invoked = False
         return {'FINISHED'}
