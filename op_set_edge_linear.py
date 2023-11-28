728eb7326363358c6e13ce360cdc51526682c9b6
Benjamin Sauder <13512160+BenjaminSauder@users.noreply.github.com>
2023-11-28 22:18:12 +0100

    set linear cleanup

--- op_set_edge_linear.py
+++ op_set_edge_linear.py
@@ -10,37 +10,13 @@ class SetEdgeLinearOP(bpy.types.Operator, op_set_edge_flow.SetEdgeLoopBase):
     bl_idname = "mesh.set_edge_linear"
     bl_label = "Set edge linear"
     bl_options = {'REGISTER', 'UNDO'}
-    bl_description = "makes edge loops linear"
+    bl_description = "Makes edge loops linear between start and end vertices"
 
     space_evenly : BoolProperty(name="Space evenly", default=False, description="Spread the vertices in even distances")
-    # distance : FloatProperty(name="Distance", default=1.0, min=0)
-
-    def draw(self, context):
-        layout = self.layout
-        column = layout.column()
-        # if self.do_straighten:
-        #     column.prop(self, "distance")
-        # else:
-        column.prop(self, "space_evenly")
-
+   
     def invoke(self, context, event):
-        #print("--------------------")
+       
         super(SetEdgeLinearOP, self).invoke(context)
-
-        # self.do_straighten = self.can_straighten()
-        # if self.do_straighten:
-        #     distance = 0
-        #     edge_count = 0
-        #     for obj in self.objects:
-        #         for edgeloop in self.edgeloops[obj]:
-        #             distance += edgeloop.get_average_distance()
-
-        #         edge_count += len(self.edgeloops[obj])
-
-        #     distance /= edge_count
-
-        #     self.distance = distance * 0.35
-
         return self.execute(context)
 
     def can_straighten(self):
@@ -53,23 +29,16 @@ class SetEdgeLinearOP(bpy.types.Operator, op_set_edge_flow.SetEdgeLoopBase):
 
     def execute(self, context):
         #print("execute")
-        
-        # if not hasattr(self, "objects") or not self.objects:
-            # return self.invoke(context, None)
+      
         if not self.is_invoked:        
             return self.invoke(context, None)
-
-        bpy.ops.object.mode_set(mode='OBJECT')
 
         bpy.ops.object.mode_set(mode='OBJECT')
 
         self.revert()
 
         for obj in self.objects:
-            for edgeloop in self.edgeloops[obj]:
-                # if self.do_straighten:
-                #     edgeloop.straighten(self.distance)
-                # else:
+            for edgeloop in self.edgeloops[obj]:               
                 edgeloop.set_linear(self.space_evenly)
 
             self.bm[obj].to_mesh(obj.data)
