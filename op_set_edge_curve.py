d7ba871c2a1016ed45c9e21b2668bd36c7247c4c
Benjamin Sauder <13512160+BenjaminSauder@users.noreply.github.com>
2023-11-28 22:18:35 +0100

    set edge curve cleanup

--- op_set_edge_curve.py
+++ op_set_edge_curve.py
@@ -5,19 +5,16 @@ from bpy.props import IntProperty, FloatProperty, EnumProperty
 
 from . import op_set_edge_flow
 
-time_last_called = 0
-
 class SetEdgeCurveOP(bpy.types.Operator, op_set_edge_flow.SetEdgeLoopBase):
 
     bl_idname = "mesh.set_edge_curve"
     bl_label = "Set edge curve"
     bl_options = {'REGISTER', 'UNDO'}
-    bl_description = "adjust edge loops to curvature"
+    bl_description = "Adjust edge loops to loop curvature\nALT: reuse last settings"
 
+    mix: FloatProperty(name="Mix", default=1.0, min=0.0, max=1.0, subtype='FACTOR', description="Interpolate between inital position and the calculated end position")
     tension : IntProperty(name="Tension", default=100, soft_min=-500, soft_max=500, description="Tension can be used to tighten up the curvature")
-    mix: FloatProperty(name="Mix", default=1.0, min=0.0, max=1.0, description="Interpolate between inital position and the calculated end position")
 
-  
     def execute(self, context):
         # print ("execute")
 
@@ -43,10 +40,7 @@ class SetEdgeCurveOP(bpy.types.Operator, op_set_edge_flow.SetEdgeLoopBase):
 
         super(SetEdgeCurveOP, self).invoke(context)
      
-        global time_last_called
-
-        if time.time() > time_last_called + op_set_edge_flow.RESET_TO_DEFAULTS_DURATION: 
-            time_last_called = time.time()
+        if event and not event.alt:
             self.tension = 100
             self.mix = 1.0          
 
